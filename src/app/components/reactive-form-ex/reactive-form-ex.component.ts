58baa0559effa48c719ecbc6181b5e3a37b0cb5f
muhammed-alateeqi1 <mu.alateeqi@gmail.com>
2025-12-12 04:59:20 +0200

    handle erros in reactive-forms

--- src/app/components/reactive-form-ex/reactive-form-ex.component.ts
+++ src/app/components/reactive-form-ex/reactive-form-ex.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-reactive-form-ex',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule ],
   templateUrl: './reactive-form-ex.component.html',
   styleUrl: './reactive-form-ex.component.css'
 })
 export class ReactiveFormExComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('' ,[Validators.required,Validators.email]),
+    password: new FormControl('',[Validators.required,Validators.minLength(6),Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)]),
+  })
 
+  
+
+  submitForm(){
+    if(this.loginForm.valid){
+      console.log("Form Data : ", this.loginForm.value);
+    }else{
+      console.log("Form Data : ", this.loginForm.errors);
+    }
+    this.clearInputs()
+  }
+  clearInputs(){
+    this.loginForm.value.email = ''
+    this.loginForm.value.password = ''
+  }
 }
